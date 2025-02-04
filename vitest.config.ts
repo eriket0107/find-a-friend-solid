f0c6b0be8482323ea0006f7c1be8e77170a4a866
Erik <ol.erik0107@gmail.com>
2025-02-04 18:19:12 -0300

    cicd

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,4 +3,10 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html"], // output to console and also generate an HTML report
+    },
+  },
 });
